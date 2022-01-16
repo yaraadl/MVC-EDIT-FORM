f2acd1ea0be57b2303eda99afdd1fe97ee6f834a
Yara Adel <65349219+yaraadl@users.noreply.github.com>
2022-01-16 19:16:44 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Student = WebApplication2.Models.Student;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        
+            public int studentid { get; set; }
+    [Display( Name="Name")]
+            public String studentname { get; set; }
+            public int age { get; set; }
+        
+    }
+
+   
+
+    
+
+}
---
+++ StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student = WebApplication2.Models.Student;
+
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> Studentlist = new List<Student>{
+       
+            new Student(){studentid=1,studentname="yara",age=20},
+            new Student(){studentid=2,studentname="alaa",age=22},
+            new Student(){studentid=3,studentname="bassant",age=21}
+        };
+  
+        //
+        // GET: /Student/
+        public ActionResult Index()
+        {
+             
+            return View(Studentlist.OrderBy(S => S.studentid).ToList());
+        }
+        
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var std = Studentlist.Where(s => s.studentid == id).FirstOrDefault();
+            return View(std);
+        
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            var Student = Studentlist.Where(S => S.studentid == std.studentid).FirstOrDefault();
+            Studentlist.Remove(Student);
+            Studentlist.Add(std);
+            return RedirectToAction("Index");
+        }
+
+
+        
+
+  
+	}
+}
